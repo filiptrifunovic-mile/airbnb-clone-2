e6ea4d605b87207231bf9da5845a845e2e51ceca
unknown <filip.trifunovic23@gmail.com>
2023-04-03 14:24:26 +0200

    toaster, google/github auth fields

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -139,6 +139,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
