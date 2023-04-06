7ab7e273f071dd7f189dfc993fd111c25ead1dd6
unknown <filip.trifunovic23@gmail.com>
2023-04-06 15:31:26 +0200

    get listing by id - action

--- app/components/HardButton.tsx
+++ app/components/HardButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HardButtonProps {
   listingId: string;
@@ -9,9 +10,10 @@ interface HardButtonProps {
 }
 
 const HardButton: React.FC<HardButtonProps> = ({ listingId, currentUser }) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
-
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
   return (
     <div
       onClick={toggleFavorite}
