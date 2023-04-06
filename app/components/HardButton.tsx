6cd168816296fb3221b92c0e7928e691bd8d1921
unknown <filip.trifunovic23@gmail.com>
2023-04-06 14:41:25 +0200

    heart button

---
+++ app/components/HardButton.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { SafeUser } from "../types";
+
+interface HardButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const HardButton: React.FC<HardButtonProps> = ({ listingId, currentUser }) => {
+  const hasFavorited = false;
+  const toggleFavorite = () => {};
+
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="
+      relative
+      hover:opacity-80
+      transition
+      cursor-pointer
+    "
+    >
+      <AiOutlineHeart
+        size={28}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
+};
+
+export default HardButton;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
@@ -28,7 +32,15 @@ export default function Home() {
          gap-8
          "
         >
-          <div>My feature listings</div>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            );
+          })}
         </div>
       </Container>
     </ClientOnly>
